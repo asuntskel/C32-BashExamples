d0dfe83255dba04a8af44c3cdab252b9102b825e
Edouard Blain-Noël <edouard.blainnoel@gmail.com>
2023-05-01 10:58:12 -0400

    ajout regex

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+
+# on utilise =~ pour dire que c'est un match regex
+# condition qui regarde si le truc entré est un format numérique
+read -p "Texte : " txt
+if [[ $txt =~ ^[0-9]+$ ]]
+then
+    echo "Numérique"
+fi
