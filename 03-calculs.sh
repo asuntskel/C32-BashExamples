e87b434ae6db913d1edc30e0101321750a067374
Edouard Blain-Noël <edouard.blainnoel@gmail.com>
2023-05-01 14:03:32 +0000

    Ajout calculs arithm

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+# echo 10+10 ne marchera pas... ça va afficher 10+10
+echo $((10+10))
+
+valeur=$((10+10))
+echo $valeur
+read -p "nb 1 : " nb1
+read -p "nb 2 : " nb2
+
+# on ajoute let pour eviter de devoir mettre les double  parenthèses...
+let resultat=$nb1*$nb2
+echo $resultat
+
+
