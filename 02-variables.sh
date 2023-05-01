f23abb32164c0d925e938c5af208ffee07b26e6c
Edouard Blain-Noël <edouard.blainnoel@gmail.com>
2023-05-01 13:50:54 +0000

    Ajout 02-variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+# déclarer une variable
+# en bash il faut pas mettre d'espaces pour affecter une variable.
+prenom=Neo
+nomf="Nom de famille"
+echo $prenom
+
+# forcer un integer (utilisé rarement)
+declare -i age=33
+
+# variable en lecture seule (constante)
+declare -r CHEMIN=/bin
+
 
