d995dbb184b956798371050f23ab1039e29f55ba
Edouard Blain-Noël <edouard.blainnoel@gmail.com>
2023-05-01 10:51:34 -0400

    ajout fonction base

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+# comme les scripts qu'on appelle avec des paramètres
+# $0 donnerait le nom de la fonction
+
+maFonction() {
+    echo "param : " $1
+}
+
+
+# pas de parenthèses à l'appel
+maFonction "salut"
+maFonction "toi"
