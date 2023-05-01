a13dfc5782e12a67c0f6e72746355f222d358e49
Edouard Blain-Noël <edouard.blainnoel@gmail.com>
2023-05-01 11:05:33 -0400

    lecture de fichier existant

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+#la commande test permet d'ajouter -e (existe)
+# permet de vrifier si le fichier existe?
+
+
+# on y passe le paramètre $1 et on check si ça existe (-e)
+# if [[ -e $1 ]]
+# -f c'est un fichier?
+# -d c'est un dossier?
+
+if test -e $1
+then
+    echo "test"
+fi
