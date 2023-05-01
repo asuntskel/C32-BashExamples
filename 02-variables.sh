456997efbfa82334bc1792e3a2e56c201eef4ba0
Edouard Blain-Noël <edouard.blainnoel@gmail.com>
2023-05-01 13:58:05 +0000

    Ajout variables.sh

--- 02-variables.sh
+++ 02-variables.sh
@@ -11,4 +11,12 @@ declare -i age=33
 # variable en lecture seule (constante)
 declare -r CHEMIN=/bin
 
+# afficher le nom du script
+echo "Nom script" $0
+
+# afficher le nombre de paramètres appelés sur le script
+echo "Nombre param" $#
+
+# afficher le premier paramètre
+echo "param1" $1
 
