ee27a5f015c5a1b244012d57ec4bbe1f15daebfc
Edouard Blain-Noël <edouard.blainnoel@gmail.com>
2023-05-01 10:43:44 -0400

    Ajout boucles

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 
+# pas besoin de mettre le $
+# les espaces sont facultatifs!!!
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+# BACKTICK `` -> executera la commande ls, et sauve ça dans variable
+liste=`ls`
+
+# For each...
+
+for fichier in $liste
+do
+	echo $fichier
+done
+
+
+
+# Si on fait une autre lettre que le o, on quitte la bouche
+
+resultat=o
+while [[ $resultat = o ]]
+do
+	read -p "Choix: " resultat
+done
+
